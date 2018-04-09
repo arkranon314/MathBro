44e9b239f04c67f12eddb9bcc944fb673fdb8a6c
arkranon314 <e.mikebravo3@gmail.com>
2018-04-09 19:24:15 +0200

    Refactor index.js

--- functions/index.js
+++ functions/index.js
@@ -4,38 +4,23 @@ exports.webhook = functions.https.onRequest((request, response) => {
 
         console.log("parameters: ", request.body.result.parameters);
         let params  = request.body.result.parameters;
-        var numbers = params.numbers;
+        let numbers = params.numbers;
         switch(request.body.result.action){
           case 'sum':
               numbers = numbers.reduce(sum,0);
-              response.send({
-                  speech:
-                      "The result is " + numbers
-              });
+              send();
             break;
           case 'subtract':
               numbers = numbers.reduce(subtract);
-              response.send({
-                  speech:
-                      "The result is " + numbers
-              });
-              console.log(response)
+              send();
             break;
           case 'multiply':
               numbers = numbers.reduce(multiply);
-              response.send({
-                  speech:
-                      "The result is " + numbers
-              });
-              console.log(response)
+              send();
             break;
           case 'divide':
               numbers = numbers.reduce(divide);
-              response.send({
-                  speech:
-                      "The result is " + numbers
-              });
-              console.log(response)
+              send();
             break;
         }
 
@@ -43,4 +28,9 @@ exports.webhook = functions.https.onRequest((request, response) => {
         function subtract(x, y){return x - y}
         function multiply(x, y){return x * y}
         function divide(x, y){return x / y}
+        function send(){
+          response.send({
+                  speech:
+                      "The result is " + numbers
+              });}
     });
