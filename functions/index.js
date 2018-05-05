5de730f17f744d31cd102c64b0c898383b9f9947
arkranon314 <e.mikebravo3@gmail.com>
2018-05-05 15:52:59 +0200

    Refactoring and question number added

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const max_limit = 10;
+const max_limit_numbers = 10;
+const questions_per_exam = 3;
 admin.initializeApp(functions.config().firebase);
 
 var db = admin.firestore();
@@ -8,10 +9,14 @@ var db = admin.firestore();
 exports.webhook = functions.https.onRequest((request, response) => {
 
         console.log("parameters: ", request.body.result.parameters);
+        console.log("action: ", request.body.result.action);
         let params  = request.body.result.parameters;
         let numbers = params.numbers;
-        console.log(request.body.result.action);
         var score = 0;
+        var questionNumber = 0;
+        var response_dict = {};
+        var parameters_dict = {};
+
         switch(request.body.result.action){
           case 'sum':
               numbers = numbers.reduce(sum,0);
@@ -29,34 +34,32 @@ exports.webhook = functions.https.onRequest((request, response) => {
               numbers = numbers.reduce(divide);
               send();
             break;
-          case 'test_basic_addition':
-              numbers1 = getRandomInt(0,max_limit)
-              numbers2 = getRandomInt(0,max_limit)
-              console.log("key " + params.score);
+           case 'test_basic_addition':
+              numbers1 = getRandomInt(0,max_limit_numbers)
+              numbers2 = getRandomInt(0,max_limit_numbers)
               if (params.score!==""){
-                  console.log("dentro del if basic addition: " + score);
+                  console.log("dentro del if basic addition: ", score);
                   score = params.score;
               }
+              if(params.questionNumber!==""){
+	          questionNumber = params.questionNumber;
+              }
+
+              parameters_dict.numbers1 = numbers1;
+              parameters_dict.numbers2 = numbers2;
+              parameters_dict.score = score;
+              parameters_dict.questionNumber = questionNumber;
+ 
               response.send({
-                  speech:
-                      `what is the result of sum ${numbers1} to ${numbers2}?`,
                   followupEvent: {
                       name: "test_mode_addition",
-                      parameters: {
-                          "numbers1": numbers1,
-			  "numbers2": numbers2,
-                          "score": score,
-                      }
+                      parameters: parameters_dict
                   },
                   contextOut: [
   		  {
   		     name: "test_mode_addition-followup",
     		     lifespan: 1,
-    		     parameters: {
-                         "numbers1": numbers1,
-                         "numbers2": numbers2,
-                         "score": score,
-    		     }
+    		     parameters: parameters_dict
  		 }
 		],
               });
@@ -67,27 +70,34 @@ exports.webhook = functions.https.onRequest((request, response) => {
 	      var numbers2 = parseInt(params.numbers2);
               var sumres = sum(numbers1,numbers2);
               score = params.score;
+              questionNumber = params.questionNumber + 1;
               console.log("mode_addition_result_score: " + score);
+              response_dict.contextOut = [
+	  		        {
+	  		            name: "test_mode",
+	    		            lifespan: 3,
+	    		            parameters: {
+		                        "score": score,
+		                        "questionNumber": questionNumber
+	     		            }
+	       		        }
+			   ];
               if (result === sumres){
                  score +=1;
-	         response.send({
-                    speech:
-                       `Correct! Do you want to go to the next question ${score}?`,
-                    contextOut: [
-  		        {
-  		        name: "test_mode",
-    		        lifespan: 3,
-    		        parameters: {
-                          "score": score,
-     		       }
-       		    }
-		   ],
-                });
+                 response_dict.contextOut[0].parameters.score = score;
+                 if (questionNumber === questions_per_exam){
+                        response_dict.speech = `Correct! You have finished the exam. Your score is ${score} out of ${questionNumber}`;   
+                 }else{
+                        response_dict.speech = `Correct! Do you want to go to the next question? current score: ${score} out of ${questionNumber}`;
+                 }
               }else{
-                 response.send({
-                    speech:
-                       "Incorrect! Do you want to go to the next question?"
-                 });}
+		if (questionNumber === questions_per_exam){
+                        response_dict.speech = `Incorrect! The correct answer is: ${sumres} .You have finished the exam. Your score is ${score} out of ${questionNumber}`;
+                }else{
+                        response_dict.speech = `Incorrect! The correct answer is: ${sumres} .Do you want to go to the next question? current score: ${score} out of ${questionNumber}`;
+                }
+	      }
+              response.send(response_dict);
             break;
           case 'test_mode_addition.next_question':
               response.send({
@@ -95,6 +105,7 @@ exports.webhook = functions.https.onRequest((request, response) => {
                       name: "init_test_addition",
                       parameters: {
                           "score": params.score,
+                          "questionNumber": params.questionNumber
                       }
                   },
               });
